062227a0ca31761abee50c43e3a0e4399dcbf5ed
Josue Ixcayau <ixca.josue@gmail.com>
2023-05-14 17:48:27 -0600

    Corrección de errores y mejoras en la lógica para obtener valores UF de forma adecuada.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime, timedelta
 from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
@@ -21,7 +22,7 @@ def get_uf(date):
             today = datetime.today()
             # Verificar que la fecha no sea más antigua de 365 días
             if date_obj < (today - timedelta(days=365)):
-                raise ValueError("La fecha consultada es demasiado antigua")
+                return jsonify({"error": "La fecha consultada es demasiado antigua"}), 400
         except ValueError as error:
             return jsonify({"error": str(error)}), 400
 
@@ -33,7 +34,24 @@ def get_uf(date):
             return jsonify({"error": "No se pudo obtener el valor de la Unidad de Fomento"}), 500
         
         html = response.content.decode("iso-8859-1")
-        value = html.split(str(date_obj.day).zfill(2))[1].split("</td>")[0].split(">")[-1]
+        # value = html.split(str(date_obj.day).zfill(2))[1].split("</td>")[0].split(">")[-1]
+        # CACHE[date] = value
+
+        # Busca la tabla que contiene los valores de la UF
+        soup = BeautifulSoup(html, "html.parser")
+        table = soup.find("div", {"id": "mes_all"}).find("table")
+
+        # Extrae los valores de la tabla
+        rows = table.find_all("tr")[1:]
+        # cols = rows[0].find_all("td")[1:]
+
+        # Busca la fila y columna correspondiente a la fecha específica
+        day = date_obj.day
+        month = date_obj.month - 1
+        value = rows[day - 1].find_all("td")[month].text.strip()
+        value = float(value.replace(".", "").replace(",", "."))
+
+        # Guarda el valor de la UF en caché
         CACHE[date] = value
 
     # Devolver la respuesta en formato JSON
