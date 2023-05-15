a2ac2f3125a79e9815366dc4ab1cc5c9c993757a
Josue Ixcayau <ixca.josue@gmail.com>
2023-05-14 19:52:27 -0600

    optimizacion de codigo ( pendiente Verificar que el día esté dentro del rango de días para ese mes y año)

--- main.py
+++ main.py
@@ -5,58 +5,66 @@ from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
+app.debug = True
 
-CACHE = {}
+# 1. Utilizar una variable constante en lugar de construir la URL en cada llamada a la API.
+UF_URL_TEMPLATE = "https://www.sii.cl/valores_y_fechas/uf/uf{}.htm"
+
+# 2. Utilizar una variable para almacenar la tabla de valores de UF por año en lugar de descargar la tabla cada vez que se realiza una llamada a la API.
+UF_TABLES = {}
+
+
+def get_uf_value(date_str: str) -> float:
+    """Obtiene el valor de la UF para la fecha especificada."""
+    try:
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("La fecha especificada no tiene el formato correcto")
+
+    today = datetime.today()
+    # Verificar que la fecha no sea más antigua de 365 días
+    if date < (today - timedelta(days=365)):
+        raise ValueError("La fecha consultada es demasiado antigua")
+
+    # Verificar si la tabla de valores de UF para el año especificado está en caché
+    year = date.year
+    if year not in UF_TABLES:
+        url = UF_URL_TEMPLATE.format(year)
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise ValueError("No se pudo obtener el valor de la Unidad de Fomento")
+
+        # Almacenar la tabla de valores de UF en caché
+        html = response.content.decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        UF_TABLES[year] = soup.find("div", {"id": "mes_all"}).find("table")
+
+    # Verificar que el año esté en la tabla
+    if year not in UF_TABLES:
+        raise ValueError("No se tienen valores de UF para el año especificado")
+
+    # Busca la fila y columna correspondiente a la fecha específica
+    day = date.day
+    month = date.month 
+    row = UF_TABLES[year].find_all("tr")[day]
+
+    # Verificar que el día esté dentro del rango de días para ese mes y año
+    # if day_int < 1 or day_int > row.find_all("td")[1].text.strip():
+    #     raise ValueError("La fecha especificada está fuera del rango de fechas para las que se tienen valores de UF")
+
+    value = row.find_all("td")[month-1].text.strip()
+    value = float(value.replace(".", "").replace(",", "."))
+    return value
+
 
 
 @app.route("/uf/<date>")
 def get_uf(date):
-    # Verificar si la fecha está en caché
-    if date in CACHE:
-        print("La fecha está en caché")
-        value = CACHE[date]
-    else:
-        # Verificar que la fecha sea válida
-        try:
-            date_obj = datetime.strptime(date, "%Y-%m-%d")
-            today = datetime.today()
-            # Verificar que la fecha no sea más antigua de 365 días
-            if date_obj < (today - timedelta(days=365)):
-                return jsonify({"error": "La fecha consultada es demasiado antigua"}), 400
-        except ValueError as error:
-            return jsonify({"error": str(error)}), 400
+    """Obtiene el valor de la UF para la fecha especificada."""
+    try:
+        print("date", date) 
+        value = get_uf_value(date)
+        return jsonify({"date":date, "value": value})
+    except ValueError as error:
+        return jsonify({"error": str(error)}), 400
 
-
-        # Obtener el valor de la Unidad de Fomento para la fecha especificada
-        url = f"https://www.sii.cl/valores_y_fechas/uf/uf{date_obj.year}.htm"
-        response = requests.get(url)
-        if response.status_code != 200:
-            return jsonify({"error": "No se pudo obtener el valor de la Unidad de Fomento"}), 500
-        
-        html = response.content.decode("iso-8859-1")
-        # value = html.split(str(date_obj.day).zfill(2))[1].split("</td>")[0].split(">")[-1]
-        # CACHE[date] = value
-
-        # Busca la tabla que contiene los valores de la UF
-        soup = BeautifulSoup(html, "html.parser")
-        table = soup.find("div", {"id": "mes_all"}).find("table")
-
-        # Extrae los valores de la tabla
-        rows = table.find_all("tr")[1:]
-        # cols = rows[0].find_all("td")[1:]
-
-        # Busca la fila y columna correspondiente a la fecha específica
-        day = date_obj.day
-        month = date_obj.month - 1
-        value = rows[day - 1].find_all("td")[month].text.strip()
-        value = float(value.replace(".", "").replace(",", "."))
-
-        # Guarda el valor de la UF en caché
-        CACHE[date] = value
-
-    # Devolver la respuesta en formato JSON
-    return jsonify({"date": date, "value": value})
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
