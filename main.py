7b1886df832ade5783b585a10d1bde1729cc820f
Josue Ixcayau <ixca.josue@gmail.com>
2023-05-17 17:29:30 -0600

    mejora en la eficiencia y del código al evitar descargas innecesarias de la tabla de valores de UF y al utilizar variables globales para almacenar y actualizar los datos relevantes.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import requests
 from datetime import datetime, timedelta
-from flask import Flask, request, jsonify
+from flask import Flask,jsonify
 from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
-app.debug = True
 
-# 1. Utilizar una variable constante en lugar de construir la URL en cada llamada a la API.
+# Utilizar una variable constante en lugar de construir la URL en cada llamada a la API.
 UF_URL_TEMPLATE = "https://www.sii.cl/valores_y_fechas/uf/uf{}.htm"
 
-# 2. Utilizar una variable para almacenar la tabla de valores de UF por año en lugar de descargar la tabla cada vez que se realiza una llamada a la API.
+# Utilizar una variable para almacenar la tabla de valores de UF por año en lugar de descargar la tabla cada vez.
 UF_TABLES = {}
+UF_MAX_UPDATED_DATE = None
 
 
 def get_uf_value(date_str: str) -> float:
@@ -22,13 +22,14 @@ def get_uf_value(date_str: str) -> float:
         raise ValueError("La fecha especificada no tiene el formato correcto")
 
     today = datetime.today()
+
     # Verificar que la fecha no sea más antigua de 365 días
     if date < (today - timedelta(days=365)):
         raise ValueError("La fecha consultada es demasiado antigua")
 
-    # Verificar si la tabla de valores de UF para el año especificado está en caché
     year = date.year
-    if year not in UF_TABLES:
+    # Verificar si la tabla de valores de UF para el año especificado está en caché
+    if year not in UF_TABLES or not is_table_up_to_date(year, date):
         url = UF_URL_TEMPLATE.format(year)
         response = requests.get(url)
         if response.status_code != 200:
@@ -37,34 +38,85 @@ def get_uf_value(date_str: str) -> float:
         # Almacenar la tabla de valores de UF en caché
         html = response.content.decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
-        UF_TABLES[year] = soup.find("div", {"id": "mes_all"}).find("table")
+        table = soup.find("div", {"id": "mes_all"}).find("table")
+        #Actualiza la tabla de valores de UF
+        uf_table = parse_table(table, year)
+        UF_TABLES[year] = uf_table
+        #Actualiza la fecha de última 
+        update_max_updated_date(uf_table)
 
     # Verificar que el año esté en la tabla
     if year not in UF_TABLES:
         raise ValueError("No se tienen valores de UF para el año especificado")
 
-    # Busca la fila y columna correspondiente a la fecha específica
-    day = date.day
-    month = date.month 
-    row = UF_TABLES[year].find_all("tr")[day]
+    # Buscar el valor de UF correspondiente a la fecha específica
+    value = find_uf_value(UF_TABLES[year], date_str)
+    if value is None:
+        raise ValueError("La fecha especificada está fuera del rango de fechas para las que se tienen valores de UF")
 
-    # Verificar que el día esté dentro del rango de días para ese mes y año
-    # if day_int < 1 or day_int > row.find_all("td")[1].text.strip():
-    #     raise ValueError("La fecha especificada está fuera del rango de fechas para las que se tienen valores de UF")
-
-    value = row.find_all("td")[month-1].text.strip()
-    value = float(value.replace(".", "").replace(",", "."))
     return value
 
 
+def is_table_up_to_date(year: int, date: datetime) -> bool:
+    """Verifica si la tabla de valores de UF para el año está actualizada."""
+    global UF_MAX_UPDATED_DATE
+    if UF_MAX_UPDATED_DATE is not None:
+        uf_last_updated_date_obj = datetime.strptime(UF_MAX_UPDATED_DATE, "%Y-%m-%d")
+        if uf_last_updated_date_obj <= date:
+            return True
+    return False
 
+
+def parse_table(table, year: int):
+    """Convierte la tabla HTML de valores de UF en un diccionario."""
+    uf_table = {}
+    rows = table.find_all("tr")
+    for row in rows[1:]:
+        cells = row.find_all()
+
+        for key_cell, cell in enumerate(cells):
+            date_str = str(year) + "-" + str(key_cell).zfill(2) + "-" + str(cells[0].text.strip()).zfill(2)
+            value = cell.text.strip().replace(".", "").replace(",", ".")
+            if is_valid_date(date_str):
+                # validar que el valor del string value no esté vacío ni sea espacio en blanco
+                stripped_value = value.strip()
+                if stripped_value:
+                    try:
+                        float_value = float(stripped_value)
+                        uf_table[date_str] = float_value
+
+                    except ValueError:
+                        pass
+
+    return uf_table
+
+
+def update_max_updated_date (uf_table):
+    """Actualiza la fecha de maxima fecha actualizada de UF si corresponde."""
+    global UF_MAX_UPDATED_DATE
+    for date_str in uf_table.keys():
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+        if UF_MAX_UPDATED_DATE is None or date_obj > datetime.strptime(UF_MAX_UPDATED_DATE, "%Y-%m-%d"):
+            UF_MAX_UPDATED_DATE = date_str
+
+
+def find_uf_value(uf_table, date: datetime) -> float:
+    """Encuentra el valor de UF correspondiente a la fecha en la tabla."""
+    if date in uf_table:
+        return uf_table[date]
+    return None
+
+def is_valid_date(date_str):
+    try:
+        datetime.strptime(date_str, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False    
 @app.route("/uf/<date>")
 def get_uf(date):
     """Obtiene el valor de la UF para la fecha especificada."""
     try:
-        print("date", date) 
         value = get_uf_value(date)
-        return jsonify({"date":date, "value": value})
+        return jsonify({"date": date, "value": value})
     except ValueError as error:
         return jsonify({"error": str(error)}), 400
-
